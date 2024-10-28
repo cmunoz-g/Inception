d24312c433e10ed574d5eeac731f45e270f0142a
Carlos <cmunozg.dev@gmail.com>
2024-10-28 14:04:20 +0100

    added vim to db container, check if .cnf is correct

--- srcs/requirements/mariadb/conf/db.sh
+++ srcs/requirements/mariadb/conf/db.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-#mkdir -p /run/mysqld
-#chown -R mysql:mysql /run/mysqld
+mkdir -p /run/mysqld
+chown -R mysql:mysql /run/mysqld
 
 #if [ ! -d "/var/lib/mysql/mysql" ]; then
 	#mysql_install_db --user=mysql --datadir=/var/lib/mysql
