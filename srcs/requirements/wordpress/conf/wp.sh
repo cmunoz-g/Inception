8cdb8fdd1cb4d457fb0e5181eaa6158cc0ecf58b
Carlos <cmunozg.dev@gmail.com>
2024-10-28 13:54:12 +0100

    fixed .sh for wordpress

--- srcs/requirements/wordpress/conf/wp.sh
+++ srcs/requirements/wordpress/conf/wp.sh
@@ -2,21 +2,28 @@
 
 # script to configure wordpress with wp-cli
 
-echo "Waiting for database"
-sleep 10 # delaying the script to make sure the database is created beforehand
+#echo "Waiting for database"
+#sleep 10 # delaying the script to make sure the database is created beforehand
+
+until mysqladmin ping -h "mariadb" --silent; do
+    echo "Waiting for database to be ready..."
+    sleep 5
+done
 
 if [ -f /var/www/wordpress/wp-config.php ]; then
 	echo "Config. file exists"
 	exit 0
 fi
 
+wp core download --allow-root --path='/var/www/wordpress'
+
 echo "Creating wp-config.php"
 wp config create --allow-root \
 --dbname="${db}" \
 --dbuser="${dbuser}" \
 --dbpass="${dbpass}" \
---dbhost=mariadb \
-#--path='/var/www/wordpress'
+--dbhost="mariadb" \
+--path='/var/www/wordpress' || echo "wp-config.php failed"
 
 # running WordPress setup: site title, admin username, email
 echo "Running WordPress installation"
@@ -24,13 +31,14 @@ wp core install --allow-root \
 --title="cmunoz-g" \
 --admin_user="cmunoz-g" \
 --admin_email="cmunoz-g@student.42madrid.com" \
---skip-email 
+--path='/var/www/wordpress' \
+--skip-email || echo "Wordpress installation failed"
 
 # creating a new user
 echo "Creating additional user"
 wp user create --allow-root \
 "cmunoz-g_clone" "noemail@email.com" \
 --role=author \
---path='/var/www/wordpress'
+--path='/var/www/wordpress' \ || echo "Additional user creation failed"
 
 echo "WordPress setup complete!"
