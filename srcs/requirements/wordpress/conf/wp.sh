564d8a6e9dd82756fde565be33b37b06b6f9177a
Carlos <cmunozg.dev@gmail.com>
2024-10-31 13:52:10 +0100

    changed wp Dockerfile and sh, it's now running

--- srcs/requirements/wordpress/conf/wp.sh
+++ srcs/requirements/wordpress/conf/wp.sh
@@ -15,22 +15,30 @@ if [ -f /var/www/wordpress/wp-config.php ]; then
 	exit 0
 fi
 
-wp core download --allow-root --path='/var/www/wordpress' || echo "Wordpress installation failed"
+cd /var/www/html
+wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar 1> /dev/null
+chmod +x wp-cli.phar
+
+./wp-cli.phar core download --allow-root --path='/var/www/wordpress' 1> /dev/null || echo "Wordpress installation failed"
 
 chown -R www-data:www-data /var/www/wordpress
 chmod -R 755 /var/www/wordpress
 
 echo "Creating wp-config.php"
-wp config create --allow-root \
+./wp-cli.phar config create --allow-root \
 --dbname="${db}" \
 --dbuser="${dbuser}" \
 --dbpass="${dbpass}" \
 --dbhost=mariadb:3306 \
---path='/var/www/wordpress' || echo "wp-config.php failed"
+--path='/var/www/wordpress' \
+--force || echo "wp-config.php creation failed"
+
+# should i consider adding wp variables in .env ?
 
 # running WordPress setup: site title, admin username, email
 echo "Running WordPress installation"
-wp core install --allow-root \
+./wp-cli.phar core install --allow-root \
+--url="cmunoz-g.42.fr" \
 --title="cmunoz-g" \
 --admin_user="cmunoz-g" \
 --admin_email="cmunoz-g@student.42madrid.com" \
@@ -39,9 +47,9 @@ wp core install --allow-root \
 
 # creating a new user
 echo "Creating additional user"
-wp user create --allow-root \
+./wp-cli.phar user create --allow-root \
 "cmunoz-g_clone" "noemail@email.com" \
 --role=author \
---path='/var/www/wordpress' \ || echo "Additional user creation failed"
+--path='/var/www/wordpress' || echo "Additional user creation failed"
 
 echo "WordPress setup complete!"
