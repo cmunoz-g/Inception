d1497f7c1d3f3aa76fa73f98617b69d05c6220d4
Carlos Munoz Gutierrez <cmunoz-g@c1r16s3.42madrid.com>
2024-11-07 12:42:27 +0100

    nginx and wordpress now in sync

--- srcs/requirements/wordpress/conf/wp.sh
+++ srcs/requirements/wordpress/conf/wp.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
-# script to configure wordpress with wp-cli
+# script to configure WordPress with wp-cli
 
-#echo "Waiting for database"
-#sleep 10 # delaying the script to make sure the database is created beforehand
-
+# Waiting for the database to be ready
 until mysqladmin ping -h "mariadb" --silent; do
     echo "Waiting for database to be ready..."
     sleep 10
 done
 
-if [ -f /var/www/wordpress/wp-config.php ]; then
+if [ -f /var/www/html/wp-config.php ]; then
 	echo "Config. file exists"
 	exit 0
 fi
@@ -19,10 +17,12 @@ cd /var/www/html
 wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar 1> /dev/null
 chmod +x wp-cli.phar
 
-./wp-cli.phar core download --allow-root --path='/var/www/wordpress' 1> /dev/null || echo "Wordpress installation failed"
+# Downloading WordPress core files into /var/www/html
+./wp-cli.phar core download --allow-root --path='/var/www/html' 1> /dev/null || echo "WordPress installation failed"
 
-chown -R www-data:www-data /var/www/wordpress
-chmod -R 755 /var/www/wordpress
+# Setting ownership and permissions
+chown -R www-data:www-data /var/www/html
+chmod -R 755 /var/www/html
 
 echo "Creating wp-config.php"
 ./wp-cli.phar config create --allow-root \
@@ -30,26 +30,24 @@ echo "Creating wp-config.php"
 --dbuser="${dbuser}" \
 --dbpass="${dbpass}" \
 --dbhost=mariadb:3306 \
---path='/var/www/wordpress' \
+--path='/var/www/html' \
 --force || echo "wp-config.php creation failed"
 
-# should i consider adding wp variables in .env ?
-
-# running WordPress setup: site title, admin username, email
+# Running WordPress setup: site title, admin username, email
 echo "Running WordPress installation"
 ./wp-cli.phar core install --allow-root \
 --url="cmunoz-g.42.fr" \
 --title="cmunoz-g" \
 --admin_user="cmunoz-g" \
 --admin_email="cmunoz-g@student.42madrid.com" \
---path='/var/www/wordpress' \
---skip-email || echo "Wordpress installation failed"
+--path='/var/www/html' \
+--skip-email || echo "WordPress installation failed"
 
-# creating a new user
+# Creating an additional user
 echo "Creating additional user"
 ./wp-cli.phar user create --allow-root \
 "cmunoz-g_clone" "noemail@email.com" \
 --role=author \
---path='/var/www/wordpress' || echo "Additional user creation failed"
+--path='/var/www/html' || echo "Additional user creation failed"
 
 echo "WordPress setup complete!"
